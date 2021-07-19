d0cdf13db0bcb9f44ce1a7aa21fbcdf4aa701357
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-07-18 21:08:16 -0300

    heroku port

--- src/server.ts
+++ src/server.ts
@@ -16,4 +16,4 @@ app.use("/api.docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is Running"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is Running"));
